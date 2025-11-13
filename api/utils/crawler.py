7b3c9bd0dacbcec9379e14aac00dda26cd64a1e4
DPC0516 <kyoungchan0516@gmail.com>
2025-11-13 19:28:32 +0900

    ueee

--- api/utils/crawler.py
+++ api/utils/crawler.py
@@ -26,11 +26,12 @@ T = TypeVar("T")
 
 def get_chrome_options():
     chrome_options = Options()
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
     chrome_options.add_experimental_option(
         "excludeSwitches", ["enable-automation"]
     )
